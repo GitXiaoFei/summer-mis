12e8c70cb701210a0ea5fbbcaa44a317b53d48c2
李远 <460289628@qq.com>
2016-10-31 16:10:27 +0800

    改进DateTimeField的默认长度，以及添加一个带长度的构造方法

--- src/main/java/cn/cerc/jpage/fields/DateTimeField.java
+++ src/main/java/cn/cerc/jpage/fields/DateTimeField.java
@@ -5,6 +5,10 @@ import cn.cerc.jpage.common.DataView;
 public class DateTimeField extends StringField {
 
 	public DateTimeField(DataView owner, String name, String field) {
-		super(owner, name, field, 14);
+		super(owner, name, field, 10);
+	}
+	
+	public DateTimeField(DataView owner, String name, String field, int width) {
+		super(owner, name, field, width);
 	}
 }
