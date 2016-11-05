5d522d45ea85245eccae6df49d062f4f5dc82576
张弓 <sz9214e@qq.com>
2016-11-05 11:58:05 +0800

    重构代码

--- src/main/java/cn/cerc/jmis/page/IMenuBar.java
+++ src/main/java/cn/cerc/jmis/page/IMenuBar.java
@@ -2,12 +2,10 @@ package cn.cerc.jmis.page;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import cn.cerc.jdb.core.IHandle;
+import cn.cerc.jbean.form.IForm;
 import cn.cerc.jpage.other.Url_Record;
 
 public interface IMenuBar {
 	// 登记菜单栏菜单项
-	public int enrollMenu(IHandle handle, HttpServletRequest request, List<Url_Record> menus);
+	public int enrollMenu(IForm form, List<Url_Record> menus);
 }
