1630019e7b81a01d05950d187e9257bb758c4381
韦善贵 <873261248@qq.com>
2016-11-24 18:01:15 +0800

    增加checkBox属性

--- src/main/java/cn/cerc/jpage/vcl/GroupBox.java
+++ src/main/java/cn/cerc/jpage/vcl/GroupBox.java
@@ -1,11 +1,9 @@
 package cn.cerc.jpage.vcl;
 
-import cn.cerc.jpage.core.ActionForm;
 import cn.cerc.jpage.core.Component;
 import cn.cerc.jpage.core.HtmlWriter;
 
 public class GroupBox extends Component {
-	private ActionForm form;
 
 	public GroupBox(Component content) {
 		super(content);
@@ -17,23 +15,7 @@ public class GroupBox extends Component {
 		if (getId() != null)
 			html.print(" id='%s' ", getId());
 		html.print(">");
-		if (form != null) {
-			html.print(String.format("<form method='%s' action='%s' id='%s'>", form.getMethod(), form.getAction(),
-					form.getId()));
-			super.output(html);
-			html.print("</form>");
-		} else {
-			super.output(html);
-		}
+		super.output(html);
 		html.println("</div>");
 	}
-
-	public ActionForm getForm() {
-		return form;
-	}
-
-	public void setForm(ActionForm form) {
-		this.form = form;
-	}
-
 }
