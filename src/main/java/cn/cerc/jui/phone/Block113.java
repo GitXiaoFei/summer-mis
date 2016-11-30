033b7ee916388ae28f0652642d7e8fe44a5b23bb
黄荣君 <18566767108@163.com>
2016-11-30 10:27:17 +0800

    增加长文本消息组件

---
+++ src/main/java/cn/cerc/jui/phone/Block113.java
@@ -0,0 +1,42 @@
+package cn.cerc.jui.phone;
+
+import cn.cerc.jpage.core.Component;
+import cn.cerc.jpage.core.HtmlWriter;
+import cn.cerc.jpage.vcl.Label;
+import cn.cerc.jpage.vcl.TextArea;
+
+public class Block113 extends Component {
+	private Label label = new Label();
+	private TextArea input = new TextArea();
+
+	/**
+	 * 文本 + 长文本消息
+	 * <p>
+	 * 显示长文本信息，例如收货地址
+	 * 
+	 * @param owner
+	 *            内容显示区
+	 */
+	public Block113(Component owner) {
+		super(owner);
+		label.setText("(label)");
+	}
+
+	@Override
+	public void output(HtmlWriter html) {
+		html.println("<!-- %s -->", this.getClass().getName());
+		html.print("<div class='block113'>");
+		label.output(html);
+		input.output(html);
+		html.println("</div>");
+	}
+
+	public Label getLabel() {
+		return label;
+	}
+
+	public TextArea getInput() {
+		return input;
+	}
+
+}
