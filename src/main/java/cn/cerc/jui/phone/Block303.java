ee33991033676538574f72077fc8bb28a37b28bd
黄荣君 <18566767108@163.com>
2016-12-13 15:13:46 +0800

    更新Block303，增加使用范例和异常处理

--- src/main/java/cn/cerc/jui/phone/Block303.java
+++ src/main/java/cn/cerc/jui/phone/Block303.java
@@ -9,13 +9,15 @@ import cn.cerc.jpage.core.UrlRecord;
 import cn.cerc.jpage.vcl.Image;
 
 /**
- * 系统菜单显示组件
+ * 可用菜单 图标 + 文字
+ * <p>
+ * 一行最多占显示4个菜单
  * 
  * @author HuangRongjun
  *
  */
 public class Block303 extends Component {
-	private Map<UrlRecord, Image> menus = new LinkedHashMap<>();
+	private Map<UrlRecord, Image> items = new LinkedHashMap<>();
 
 	public Block303(Component owner) {
 		super(owner);
@@ -23,40 +25,40 @@ public class Block303 extends Component {
 
 	@Override
 	public void output(HtmlWriter html) {
+		if (items.size() == 0) {
+			for (int i = 0; i < 4; i++) {
+				UrlRecord url = new UrlRecord();
+				url.setName("(名称)");
+				url.setSite("#");
+				Image img = new Image();
+				img.setSrc("jui/phone/block301-leftIcon.png");
+				this.addItem(url, img);
+			}
+		}
+
 		html.println("<!-- %s -->", this.getClass().getName());
-		html.println("<div class='bolcokMenu' role='row'>");
-
-		html.println("<ul id='Board1' class='menuBoard'>");
-		if (menus.size() == 0) {
-			html.println("<li>");
-			html.println("	<div class='row1'>");
-			html.println("		<img src='jui/phone/block301-leftIcon.png'>");
-			html.println("	</div>");
-			html.println("	<div class='row2'>");
-			html.println("		菜单名称");
-			html.println("	</div>");
-			html.println("</li>");
+		html.println("<div class='block303'>");
+		for (UrlRecord url : items.keySet()) {
+			html.println("<div role='item'>");
+			html.println("<div role='image'>");
+			html.println("<a href='%s'>%s</a>", url.getUrl(), items.get(url));
+			html.println("</div>");
+			html.println("<div role='title'>");
+			html.println("<a href='%s'>%s</a>", url.getUrl(), url.getName());
+			html.println("</div>");
+			html.println("</div>");
 		}
+		html.println("</div>");
+	}
 
-		for (UrlRecord url : menus.keySet()) {
-			html.println("<li>");
-			html.println("	<div class='row1'>");
-			html.println("		<a href='%s'>%s</a>", url.getUrl(), menus.get(url));
-			html.println("	</div>");
-			html.println("	<div class='row2'>");
-			html.println("		<a href='%s'>%s</a>", url.getUrl(), url.getName());
-			html.println("	</div>");
-			html.println("</li>");
+	public void addItem(UrlRecord url, Image image) {
+		if (items.size() > 3) {
+			throw new RuntimeException("一个菜单组件最多容纳4个对象");
 		}
-		html.println("</ul>");
+		items.put(url, image);
 	}
 
-	public void addMenu(UrlRecord url, Image image) {
-		menus.put(url, image);
+	public int size() {
+		return items.size();
 	}
-
-	public Map<UrlRecord, Image> getMenus() {
-		return menus;
-	}
-
 }
