9a53e880be9a3c032c6cc90a6f9ea66145e80657
GuoXiangSheng <757618411@qq.com>
2016-12-15 20:23:42 +0800

    全部block展示

--- src/main/java/cn/cerc/jui/phone/FrmPhoneSample.java
+++ src/main/java/cn/cerc/jui/phone/FrmPhoneSample.java
@@ -9,26 +9,39 @@ public class FrmPhoneSample extends AbstractForm {
 	public IPage execute() throws Exception {
 		PhonePage page = new PhonePage(this);
 
-		new Block101(page.getContent());
+	    new Block101(page.getContent());
 		new Block102(page.getContent());
 		new Block103(page.getContent());
 		new Block104(page.getContent());
 		new Block105(page.getContent());
-		new Block112(page.getContent());
+		new Block106(page.getContent());
 		new Block107(page.getContent());
+		new Block108(page.getContent());
+		new Block109(page.getContent());
+		new Block110(page.getContent());
+		new Block111(page.getContent());
+		new Block112(page.getContent());
+		new Block113(page.getContent());
+		new Block114(page.getContent());
+		new Block115(page.getContent());
+		new Block116(page.getContent());
+		new Block117(page.getContent());
 		new Block118(page.getContent());
-
+		
 		new Block301(page.getContent());
-		new Block302(page.getContent());
-		new Block303(page.getContent());
-		new Block304(page.getContent());
-
+		
 		new Block401(page.getContent());
 		new Block402(page.getContent());
 
+		new Block601(page.getContent());
+		new Block602(page.getContent());
+		new Block603(page.getContent());
+
+		
+		
 		new Block901(page.getContent());
 
-		// new Block991(page.getContent());
+		new Block991(page.getContent());
 		Block992 b992 = new Block992(page.getContent());
 		b992.addButton("进入系统");
 		b992.addButton("进入系统");
