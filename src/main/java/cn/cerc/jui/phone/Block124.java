b2d646956ce93dc8eb464a16fc1068305724de35
GuoXiangSheng <757618411@qq.com>
2016-12-16 15:11:58 +0800

    增加125模板与样式，用来显示item列表项

--- src/main/java/cn/cerc/jui/phone/Block124.java
+++ src/main/java/cn/cerc/jui/phone/Block124.java
@@ -2,9 +2,7 @@ package cn.cerc.jui.phone;
 
 import cn.cerc.jpage.core.Component;
 import cn.cerc.jpage.core.HtmlWriter;
-import cn.cerc.jpage.core.UrlRecord;
 import cn.cerc.jpage.vcl.Span;
-import cn.cerc.jpage.vcl.TextBox;
 
 /**
  * 提示块
