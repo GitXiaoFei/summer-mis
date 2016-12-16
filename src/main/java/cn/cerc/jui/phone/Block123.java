af01571bf85c47e56866f1e4c351b9918cfc3c5e
GuoXiangSheng <757618411@qq.com>
2016-12-16 14:09:47 +0800

    增加块123

---
+++ src/main/java/cn/cerc/jui/phone/Block123.java
@@ -0,0 +1,66 @@
+package cn.cerc.jui.phone;
+
+import cn.cerc.jpage.core.Component;
+import cn.cerc.jpage.core.HtmlWriter;
+import cn.cerc.jpage.core.UrlRecord;
+import cn.cerc.jpage.vcl.Span;
+import cn.cerc.jpage.vcl.TextBox;
+
+/**
+ * 一组左边图标右边文字
+ * 
+ * @author 郭向军
+ *
+ */
+public class Block123 extends Component {
+	private Span title = new Span();
+	private UrlRecord urlRecord = new UrlRecord();
+	private TextBox textBox = new TextBox();
+
+	public TextBox getTextBox() {
+		return textBox;
+	}
+
+	public void setTextBox(TextBox textBox) {
+		this.textBox = textBox;
+	}
+
+	public Block123(Component owner) {
+		super(owner);
+		this.textBox.setMaxlength("20");
+		this.textBox.setPlaceholder("请输入");
+		this.textBox.setType("text");
+	}
+
+	public void setTitle(Span title) {
+		this.title = title;
+	}
+
+	@Override
+	public void output(HtmlWriter html) {
+		html.println("<!-- %s -->", this.getClass().getName());
+		html.print("<div class='block123'>");
+		textBox.output(html);
+		html.print("<a href='%s' class='save'>", this.urlRecord.getUrl());
+		this.title.output(html);
+		html.print("</a>");
+		html.print("</div>");
+	}
+
+	public Span getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title.setText(title);
+	}
+
+	public UrlRecord getUrlRecord() {
+		return urlRecord;
+	}
+
+	public void setUrlRecord(UrlRecord urlRecord) {
+		this.urlRecord = urlRecord;
+	}
+
+}
